d4ad4c277019b29161a0d5034d2a615c68115932
Vitali Plagov <vplagov@pm.me>
2026-01-15 21:42:13 +0200

    Implement password hashing

---
+++ FuelTracker.API/Security/PasswordHasher.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace FuelTracker.API.Security;
+
+public class PasswordHasher
+{
+    private const int KeySize = 64;
+
+    public static string HashPassword(string password)
+    {
+        const int iterations = 350_000;
+        var salt = RandomNumberGenerator.GetBytes(KeySize);
+        var hash = Rfc2898DeriveBytes.Pbkdf2(Encoding.UTF8.GetBytes(password), salt, iterations,
+            HashAlgorithmName.SHA512, KeySize);
+        return $"{iterations}.{Convert.ToBase64String(salt)}.{Convert.ToBase64String(hash)}";
+    }
+
+    public static bool VerifyHashedPassword(string password, string combinedHash)
+    {
+        var parts = combinedHash.Split(".");
+        var iterations = int.Parse(parts[0]);
+        var salt = Convert.FromBase64String(parts[1]);
+        var hashedPassword = Convert.FromBase64String(parts[2]);
+
+        var hashToCompare = Rfc2898DeriveBytes.Pbkdf2(password, salt, iterations, HashAlgorithmName.SHA512, KeySize);
+        return CryptographicOperations.FixedTimeEquals(hashToCompare, hashedPassword);
+    }
+}
